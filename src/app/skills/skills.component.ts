b40101e600a72138dbf9404c8b25fb2ac23bf1ec
Glebova Diana <glebova.di5@gmail.com>
2019-07-30 19:20:25 +0300

    clear code

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Chart} from "angular-highcharts";
+import * as Highcharts from 'highcharts';
+
 
 @Component({
   selector: 'app-skills',
@@ -7,105 +8,87 @@ import {Chart} from "angular-highcharts";
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
-  //
-  // // @ts-ignore
-  // // @ts-ignore
-  // // @ts-ignore
-  // // @ts-ignore
-  // chart = new Chart({
-  //   chart: {
-  //     type: 'area'
-  //   },
-  //   title: {
-  //     text: ''
-  //   },
-  //   // subtitle: {
-  //   //   // text: 'Sources: <a href="https://thebulletin.org/2006/july/global-nuclear-stockpiles-1945-2006">' +
-  //   //   //   'thebulletin.org</a> &amp; <a href="https://www.armscontrol.org/factsheets/Nuclearweaponswhohaswhat">' +
-  //   //   //   'armscontrol.org</a>'
-  //   // },
-  //   xAxis: {
-  //     title: {
-  //       text: ''
-  //     },
-  //     allowDecimals: false,
-  //     labels: {
-  //       formatter: function () {
-  //         return this.value; // clean, unformatted number for year
-  //       }
-  //     }
-  //   },
-  //   yAxis: {
-  //     title: {
-  //       text: ''
-  //     },
-  //     labels: {
-  //       formatter: function () {
-  //         return this.value / 5 + '%';
-  //       }
-  //     }
-  //   },
-  //   tooltip: {
-  //     pointFormat: '{series.name} had stockpiled <b>{point.y:,.0f}</b><br/>warheads in {point.x}'
-  //   },
-  //   plotOptions: {
-  //     area: {
-  //       pointStart: 1940,
-  //       marker: {
-  //         enabled: false,
-  //         symbol: 'circle',
-  //         radius: 2,
-  //         states: {
-  //           hover: {
-  //             enabled: true
-  //           }
-  //         }
-  //       }
-  //     }
-  //   },
-  //   seriesOptions: [{
-  //     name: 'XD',
-  //     data: [
-  //       0, 10, 15, 20, 30, 40, 50, 60, 70, 80, 83,
-  //       70, 65, 60, 58,
-  //       60, 65, 70, 74,
-  //       70, 65, 60, 56,
-  //       60, 65, 70, 75, 80, 85, 90, 96,
-  //       90, 85, 80, 75, 70, 65, 60, 55,
-  //       60,  70, 82,
-  //       75, 65, 55, 45, 35, 25, 15, 5, 0
-  //       // null, null, null, null, null, 6, 11, 32, 110, 235,
-  //       // 369, 640, 1005, 1436, 2063, 3057, 4618, 6444, 9822, 15468,
-  //       // 20434, 24126, 27387, 29459, 31056, 31982, 32040, 31233, 29224, 27342,
-  //       // 26662, 26956, 27912, 28999, 28965, 27826, 25579, 25722, 24826, 24605,
-  //       // 24304, 23464, 23708, 24099, 24357, 24237, 24401, 24344, 23586, 22380,
-  //       // 21004, 17287, 14747, 13076, 12555, 12144, 11009, 10950, 10871, 10824,
-  //       // 10577, 10527, 10475, 10421, 10358, 10295, 10104, 9914, 9620, 9326,
-  //       // 5113, 5113, 4954, 4804, 4761, 4717, 4368, 4018
-  //     ]
-  //   },
-  //   //   {
-  //   //   name: 'USSR/Russia',
-  //   //   data: [null, null, null, null, null, null, null, null, null, null,
-  //   //     5, 25, 50, 120, 150, 200, 426, 660, 869, 1060,
-  //   //     1605, 2471, 3322, 4238, 5221, 6129, 7089, 8339, 9399, 10538,
-  //   //     11643, 13092, 14478, 15915, 17385, 19055, 21205, 23044, 25393, 27935,
-  //   //     30062, 32049, 33952, 35804, 37431, 39197, 45000, 43000, 41000, 39000,
-  //   //     37000, 35000, 33000, 31000, 29000, 27000, 25000, 24000, 23000, 22000,
-  //   //     21000, 20000, 19000, 18000, 18000, 17000, 16000, 15537, 14162, 12787,
-  //   //     12600, 11400, 5500, 4512, 4502, 4502, 4500, 4500
-  //   //   ]
-  //   // }
-  //   ]
-  // });
-  // //
-  // // // add point to chart serie
-  // // add() {
-  // //   this.chart.addPoint(Math.floor(Math.random() * 10));
 
-  constructor() { }
+  public options: any = {
+    chart: {
+      type: 'areaspline',
+      backgroundColor: '#fafafa'
+    },
+    plotOptions: {
+      series: {
+        animation: {
+          duration: 1000
+        }
+      }
+    },
+    credits: {
+      enabled: false
+    },
+    title: {
+      text: ''
+    },
+    subtitle: {
+      text: ''
+    },
+    xAxis: {
+      allowDecimals: false,
+      labels: {
+        useHTML: true,
+        formatter: function() {
+          if(this.value === 1)
+            return '<img src="../../assets/img/xd-skills.png" style="width: 26px; vertical-align: middle"/>';
+          else if(this.value === 3)
+            return '<img src="../../assets/img/ae-skills.png" style="width: 26px; vertical-align: middle" />';
+          else if (this.value === 5)
+            return '<img src="../../assets/img/ps-skills.png" style="width: 26px; vertical-align: middle"/>';
+          else if (this.value === 7)
+            return '<img src="../../assets/img/ai-skills.png" style="width: 26px; vertical-align: middle"/>';
+          else
+            return this.value;
+        }
+      },
+    },
+    yAxis: {
+      title: {
+        text: ''
+      },
+      labels: {
+        formatter: function () {
+          // return this.isFirst = 50 + '%';
+          return this.isLast = 100 + '%';
+          // this.value /1 + '%';
+        }
+      }
+    },
+    tooltip: {
+      enabled: false
+    },
+    series: [{
+      showInLegend: false,
+      // shadow: '#000000',
+      data: [
+              // 0, 10, 15, 20, 30, 40, 50, 60, 70, 80, 83,
+        0, 83, 58, 74, 56, 96, 55, 82, 0
+      // 70, 65, 60, 58,
+      // 60, 65, 70, 74,
+      // 70, 65, 60, 56,
+      // 60, 65, 70, 75, 80, 85, 90, 96,
+      // 90, 85, 80, 75, 70, 65, 60, 55,
+      // 60,  70, 82,
+      // 75, 65, 55, 45, 35, 25, 15, 5, 0
+
+      ],
+      color: '#cccccc',
+    },]
+  };
+
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+    Highcharts.chart('container', this.options);
   }
 
 }
